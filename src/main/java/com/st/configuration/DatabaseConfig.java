2797d7508342ba456893fe46e4152095a7878b5a
sergiot <sergiotorres81@gmail.com>
2016-02-23 00:56:58 +0200

    added configuration for DB based in the local environment

--- src/main/java/com/st/configuration/DatabaseConfig.java
+++ src/main/java/com/st/configuration/DatabaseConfig.java
@@ -12,8 +12,15 @@ import org.springframework.context.annotation.Primary;
 public class DatabaseConfig {
 	@Bean
 	@Primary
-	@ConfigurationProperties(prefix = "spring.datasource")
+	@ConfigurationProperties(prefix = "SPRING_DATASOURCE")
 	public DataSource dataSource() {
-		return DataSourceBuilder.create().build();
+		// TODO: right now there are 2 configurations for the local environment
+		// application.properties --> spring-boots reads from here
+		// .env --> heroku local reads from here
+		// The idea is to point Spring boot properties to .env
+		return DataSourceBuilder.create().driverClassName(System.getenv("SPRING_DATASOURCE_DIRVER-CLASS-NAME"))
+				.url(System.getenv("SPRING_DATASOURCE_URL")).username(System.getenv("SPRING_DATASOURCE_USERNAME"))
+				.password(System.getenv("SPRING_DATASOURCE_PASSWORD")).build();
+		// return DataSourceBuilder.create().build();
 	}
 }

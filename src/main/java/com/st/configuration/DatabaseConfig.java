321b91045c1572ebf984e5415eafef91deb95af7
sergiotorres81 <sergiotorres81@gmail.com>
2016-02-23 14:47:39 +0200

    changed properties to fit Heroku needs

--- src/main/java/com/st/configuration/DatabaseConfig.java
+++ src/main/java/com/st/configuration/DatabaseConfig.java
@@ -1,26 +1,72 @@
 package com.st.configuration;
 
-import javax.sql.DataSource;
 
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class DatabaseConfig {
+
+	// @Bean
+	// @Primary
+	// // @ConfigurationProperties(prefix = "SPRING_DATASOURCE")
+	// public DataSource dataSource() {
+	// // TODO: right now there are 2 configurations for the local environment
+	// // application.properties --> spring-boots reads from here
+	// // .env --> heroku local reads from here
+	// // The idea is to point Spring boot properties to .env
+	// String JDBC_DATABASE_URL = System.getenv().get("JDBC_DATABASE_URL");
+	// System.out.println("JDBC_DATABASE_URL: " + JDBC_DATABASE_URL);
+	// String driverClassName =
+	// System.getenv().get("SPRING_DATASOURCE_DIRVER-CLASS-NAME");
+	// String url = System.getenv("SPRING_DATASOURCE_URL");
+	// String username = System.getenv("SPRING_DATASOURCE_USERNAME");
+	// String password =System.getenv("SPRING_DATASOURCE_PASSWORD");
+	// System.out.println("driverClassName: " + driverClassName);
+	// System.out.println("url: " + url);
+	// System.out.println("username: " + username);
+	// System.out.println("password: " + password);
+	//
+	//
+	// return (DataSource) DataSourceBuilder.create()
+	// .driverClassName(System.getenv("SPRING_DATASOURCE_DIRVER-CLASS-NAME"))
+	// .url(System.getenv("SPRING_DATASOURCE_URL")).username(System.getenv("SPRING_DATASOURCE_USERNAME"))
+	// .password(System.getenv("SPRING_DATASOURCE_PASSWORD")).build();
+	// // return DataSourceBuilder.create().build();
+	// }
+
 	@Bean
-	@Primary
-	@ConfigurationProperties(prefix = "SPRING_DATASOURCE")
-	public DataSource dataSource() {
-		// TODO: right now there are 2 configurations for the local environment
-		// application.properties --> spring-boots reads from here
-		// .env --> heroku local reads from here
-		// The idea is to point Spring boot properties to .env
-		return DataSourceBuilder.create().driverClassName(System.getenv("SPRING_DATASOURCE_DIRVER-CLASS-NAME"))
-				.url(System.getenv("SPRING_DATASOURCE_URL")).username(System.getenv("SPRING_DATASOURCE_USERNAME"))
-				.password(System.getenv("SPRING_DATASOURCE_PASSWORD")).build();
-		// return DataSourceBuilder.create().build();
+	public DataSource postgresDataSource() {
+		String databaseUrl = System.getenv("DATABASE_URL");
+
+		System.out.println("databaseUrl: " + databaseUrl);
+		URI dbUri;
+		try {
+			dbUri = new URI(databaseUrl);
+		} catch (URISyntaxException e) {
+			return null;
+		}
+
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+		System.out.println("dbUrl: " + dbUrl);
+
+		org.apache.tomcat.jdbc.pool.DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl(dbUrl);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+		dataSource.setTestOnBorrow(true);
+		dataSource.setTestWhileIdle(true);
+		dataSource.setTestOnReturn(true);
+		dataSource.setValidationQuery("SELECT 1");
+		return dataSource;
 	}
+
 }

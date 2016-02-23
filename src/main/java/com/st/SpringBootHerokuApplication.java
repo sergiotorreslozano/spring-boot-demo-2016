36ed1e01be0d54ddb051923b5407374b04a484e4
sergiotorres81 <sergiotorres81@gmail.com>
2016-02-23 16:23:08 +0200

    added log4j

--- src/main/java/com/st/SpringBootHerokuApplication.java
+++ src/main/java/com/st/SpringBootHerokuApplication.java
@@ -1,12 +1,19 @@
 package com.st;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootHerokuApplication {
 
+	private final static Logger logger = LoggerFactory.getLogger(SpringBootHerokuApplication.class);
+
 	public static void main(String[] args) {
+		logger.debug("Starting SpringBootHerokuApplication...");
 		SpringApplication.run(SpringBootHerokuApplication.class, args);
+		logger.debug("Configuration finished for SpringBootHerokuApplication ");
+
 	}
 }
--- src/main/java/com/st/configuration/DatabaseConfig.java
+++ src/main/java/com/st/configuration/DatabaseConfig.java
@@ -5,49 +5,25 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.apache.tomcat.jdbc.pool.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DatabaseConfig {
 
-	// @Bean
-	// @Primary
-	// // @ConfigurationProperties(prefix = "SPRING_DATASOURCE")
-	// public DataSource dataSource() {
-	// // TODO: right now there are 2 configurations for the local environment
-	// // application.properties --> spring-boots reads from here
-	// // .env --> heroku local reads from here
-	// // The idea is to point Spring boot properties to .env
-	// String JDBC_DATABASE_URL = System.getenv().get("JDBC_DATABASE_URL");
-	// System.out.println("JDBC_DATABASE_URL: " + JDBC_DATABASE_URL);
-	// String driverClassName =
-	// System.getenv().get("SPRING_DATASOURCE_DIRVER-CLASS-NAME");
-	// String url = System.getenv("SPRING_DATASOURCE_URL");
-	// String username = System.getenv("SPRING_DATASOURCE_USERNAME");
-	// String password =System.getenv("SPRING_DATASOURCE_PASSWORD");
-	// System.out.println("driverClassName: " + driverClassName);
-	// System.out.println("url: " + url);
-	// System.out.println("username: " + username);
-	// System.out.println("password: " + password);
-	//
-	//
-	// return (DataSource) DataSourceBuilder.create()
-	// .driverClassName(System.getenv("SPRING_DATASOURCE_DIRVER-CLASS-NAME"))
-	// .url(System.getenv("SPRING_DATASOURCE_URL")).username(System.getenv("SPRING_DATASOURCE_USERNAME"))
-	// .password(System.getenv("SPRING_DATASOURCE_PASSWORD")).build();
-	// // return DataSourceBuilder.create().build();
-	// }
+	private final static Logger logger = LoggerFactory.getLogger(DatabaseConfig.class);
 
 	@Bean
 	public DataSource postgresDataSource() {
 		String databaseUrl = System.getenv("DATABASE_URL");
-
-		System.out.println("databaseUrl: " + databaseUrl);
+		logger.debug("Found environment variable DATABASE_URL with value: " + databaseUrl);
 		URI dbUri;
 		try {
 			dbUri = new URI(databaseUrl);
 		} catch (URISyntaxException e) {
+			logger.error("databaseUrl cannot be null");
 			return null;
 		}
 
@@ -55,7 +31,9 @@ public class DatabaseConfig {
 		String password = dbUri.getUserInfo().split(":")[1];
 		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
-		System.out.println("dbUrl: " + dbUrl);
+		logger.debug("Data base properties");
+		logger.debug("dbUrl: " + dbUrl);
+		logger.debug("username: " + username);
 
 		org.apache.tomcat.jdbc.pool.DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
 		dataSource.setDriverClassName("org.postgresql.Driver");
